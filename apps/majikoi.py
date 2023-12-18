699b50c720a4dbfc7a6e6c37a0826a4f2caf286c
ew0345 <ew0345@users.noreply.github.com>
2023-12-18 18:41:41 -0500

    A-4 added

--- apps/majikoi.py
+++ apps/majikoi.py
@@ -15,66 +15,45 @@ def writeStates():
     # Majikoi TODO
     # Majikoi S TODO
     # Majikoi A-1
-    if mA1BenkiVal.get() == 0:
-        mA1States[0] = 0
-    else:
-        mA1States[0] = 1
-
-    if mA1AzumiVal.get() == 0:
-        mA1States[1] = 0
-    else:
-        mA1States[1] = 1
-
-    if mA1SayakaVal.get() == 0:
-        mA1States[2] = 0
-    else:
-        mA1States[2] = 1
+    mA1Vars = [mA1BenkiVal, mA1AzumiVal, mA1SayakaVal]
+    
+    for i, var in enumerate(mA1Vars):
+        mA1States[i] = 0 if var.get() == 0 else 1
 
     with open(mA1State, "w") as f:
         for item in mA1States:
             f.write("%s\n" % item)
 
     # Majikoi A-2
-    if mA2MonshiroVal.get() == 0:
-        mA2States[0] = 0
-    else:
-        mA2States[0] = 1
-    
-    if mA2AiessVal.get() == 0:
-        mA2States[1] = 0
-    else:
-        mA2States[1] = 1
-    
-    if mA2SeisoVal.get() == 0:
-        mA2States[2] = 0
-    else:
-        mA2States[2] = 1
+    mA2Vars = [mA2MonshiroVal, mA2AiessVal, mA2SeisoVal]
+
+    for i, var in enumerate(mA2Vars):
+        mA2States[i] = 0 if var.get() == 0 else 1
     
     with open(mA2State, "w") as f:
         for item in mA2States:
             f.write("%s\n" % item)
 
     # Majikoi A-3
-    if mA3LeeVal.get() == 0:
-        mA3States[0] = 0
-    else:
-        mA3States[0] = 1
-    
-    if mA3StacyVal.get() == 0:
-        mA3States[1] = 0
-    else:
-        mA3States[1] = 1
-    
-    if mA3TsubameVal.get() == 0:
-        mA3States[2] = 0
-    else:
-        mA3States[2] = 1
+    mA3Vars = [mA3LeeVal, mA3StacyVal, mA3TsubameVal]
+
+    for i, var in enumerate(mA3Vars):
+        mA3States[i] = 0 if var.get() == 0 else 1
 
     with open(mA3State, "w") as f:
         for item in mA3States:
             f.write("%s\n" % item)
     
-    # Majikoi A-4 TODO
+    # Majikoi A-4 
+    mA4Vars = [mA4LinVal, mA4HomuraVal]
+
+    for i, var in enumerate(mA4Vars):
+        mA4States[i] = 0 if var.get() == 0 else 1
+
+    with open(mA4State, "w") as f:
+        for item in mA4States:
+            f.write("%s\n" % item)
+
     # Majikoi A-5 TODO
 
     root.quit()
@@ -120,20 +99,10 @@ def readStates():
         with open(mA1State, "r") as f:
             mA1States = [line.strip() for line in f.readlines()]
 
-    if int(mA1States[0]) == 0:
-        mA1BenkiVal.set(0)
-    else:
-        mA1BenkiVal.set(1)
+    mA1Vars = [mA1BenkiVal, mA1AzumiVal, mA1SayakaVal]
 
-    if int(mA1States[1]) == 0:
-        mA1AzumiVal.set(0)
-    else:
-        mA1AzumiVal.set(1)
-
-    if int(mA1States[2]) == 0:
-        mA1SayakaVal.set(0)
-    else:
-        mA1SayakaVal.set(1)
+    for i, var in enumerate(mA1Vars):
+        var.set(0 if int(mA1States[i]) == 0 else 1)
     
     # Majikoi A-2
     global mA2States
@@ -146,20 +115,10 @@ def readStates():
         with open(mA2State, "r") as f:
             mA2States = [line.strip() for line in f.readlines()]
 
-    if int(mA2States[0]) == 0:
-        mA2MonshiroVal.set(0)
-    else:
-        mA2MonshiroVal.set(1)
+    mA2Vars = [mA2MonshiroVal, mA2AiessVal, mA2SeisoVal]
 
-    if int(mA2States[1]) == 0:
-        mA2AiessVal.set(0)
-    else:
-        mA2AiessVal.set(1)
-    
-    if int(mA2States[2]) == 0:
-        mA2SeisoVal.set(0)
-    else:
-        mA2SeisoVal.set(1)
+    for i, var in enumerate(mA2Vars):
+        var.set(0 if int(mA2States[i]) == 0 else 1)
     
     # Majikoi A-3
     global mA3States
@@ -172,20 +131,10 @@ def readStates():
         with open(mA3State, "r") as f:
             mA3States = [line.strip() for line in f.readlines()]
     
-    if int(mA3States[0]) == 0:
-        mA3LeeVal.set(0)
-    else:
-        mA3LeeVal.set(1)
-    
-    if int(mA3States[1]) == 0:
-        mA3StacyVal.set(0)
-    else:
-        mA3StacyVal.set(1)
-    
-    if int(mA3States[2]) == 0:
-        mA3TsubameVal.set(0)
-    else:
-        mA3TsubameVal.set(1)
+    mA3Vars = [mA3LeeVal, mA3StacyVal, mA3TsubameVal]
+
+    for i, var in enumerate(mA3Vars):
+        var.set(0 if int(mA3States[i]) == 0 else 1)
     
     # Majikoi A-4
     global mA4States
@@ -198,6 +147,11 @@ def readStates():
         with open(mA4State, "r") as f:
             mA4States = [line.strip() for line in f.readlines()]
 
+    mA4Vars = [mA4LinVal, mA4HomuraVal]
+
+    for i, var in enumerate(mA4Vars):
+        var.set(0 if int(mA4States[i]) == 0 else 1)
+
     # Majikoi A-5
     global mA5States
     if not os.path.isfile(mA5State):
@@ -208,6 +162,11 @@ def readStates():
     elif os.path.isfile(mA5State):
         with open(mA5State, "r") as f:
             mA5States = [line.strip() for line in f.readlines()]
+
+    mA5Vars = [mA5YoshitsuneVal, mA5TakaeVal, mA5MargitVal]
+
+    for i, var in enumerate(mA5Vars):
+        var.set(0 if int(mA5States[i]) == 0 else 1)
 
 def init():
     global root
@@ -285,7 +244,7 @@ def init():
     mA1Sayaka = tk.Checkbutton(tabA1, text="Completed", font=("Tahoma", 11), variable=mA1SayakaVal)
     mA1Sayaka.place(x=269, y=34)
 
-    # Majikoi A-2 TODO
+    # Majikoi A-2
     # Variables
     global mA2MonshiroVal; mA2MonshiroVal = tk.IntVar()
     global mA2AiessVal; mA2AiessVal = tk.IntVar()
@@ -316,6 +275,7 @@ def init():
     global mA3LeeVal; mA3LeeVal = tk.IntVar()
     global mA3StacyVal; mA3StacyVal = tk.IntVar()
     global mA3TsubameVal; mA3TsubameVal = tk.IntVar()
+
     # Labels
     labelLee = tk.Label(tabA3, text="Lee", font=("Tahoma", 13))
     labelLee.place(x=10, y=10)
@@ -325,6 +285,7 @@ def init():
 
     labelTsubame = tk.Label(tabA3, text="Tsubame", font=("Tahoma", 13))
     labelTsubame.place(x=270, y=10)
+
     # Checkbuttons
     mA3Lee = tk.Checkbutton(tabA3, text="Completed", font=("Tahoma", 11), variable=mA3LeeVal)
     mA3Lee.place(x=8, y=34)
@@ -334,17 +295,32 @@ def init():
 
     mA3Tsubame = tk.Checkbutton(tabA3, text="Completed", font=("Tahoma", 11), variable=mA3TsubameVal)
     mA3Tsubame.place(x=269, y=34)
-    # Majikoi A-4 TODO
+
+    # Majikoi A-4
     # Variables
     global mA4LinVal; mA4LinVal = tk.IntVar()
     global mA4HomuraVal; mA4HomuraVal = tk.IntVar()
+
     # Labels
+    labelLin = tk.Label(tabA4, text="Lin", font=("Tahoma", 13))
+    labelLin.place(x=10, y=10)
+
+    labelHomura = tk.Label(tabA4, text="Homura", font=("Tahoma", 13))
+    labelHomura.place(x=147, y=10)
+
     # Checkbuttons
+    mA4Lin = tk.Checkbutton(tabA4, text="Completed", font=("Tahoma", 11), variable=mA4LinVal)
+    mA4Lin.place(x=8, y=34)
+
+    mA4Homura = tk.Checkbutton(tabA4, text="Complete", font=("Tahoma", 11), variable=mA4HomuraVal)
+    mA4Homura.place(x=147, y=34)
+
     # Majikoi A-5 TODO
     # Variables
     global mA5YoshitsuneVal; mA5YoshitsuneVal = tk.IntVar()
     global mA5TakaeVal; mA5TakaeVal = tk.IntVar()
     global mA5MargitVal; mA5MargitVal = tk.IntVar()
+    
     # Labels
     # Checkbuttons
     
