b9179d69ca15621d160687549ce785bc6780d0ad
ew0345 <ew0345@users.noreply.github.com>
2023-12-18 01:27:56 -0500

    Launcher is fully finished now Finished launcher (launcher.py) minus any future additions to applications available

--- launcher.py
+++ launcher.py
@@ -1,6 +1,17 @@
 import tkinter as tk
+import subprocess
+
+def open(app):
+    # What application to open when a button is clicked
+    apps = {0: "scc.py", 1: "archcalc.py"}
+    try:
+        subprocess.Popen(["py", apps[app]])
+        root.quit()
+    except KeyError:
+        print(f"{app} is an invalid application id.")
 
 def init():
+    global root
     root = tk.Tk()
     # Set Window Size
     root.geometry("253x215")
@@ -29,7 +40,11 @@ def init():
     labelMadeBy.place(x=150, y=89)
 
     # Buttons
-    
+    buttonSCC = tk.Button(root, text="Sheer Cold Calculator", width=18, command=lambda: open(0))
+    buttonSCC.place(x=10, y=30)
+
+    buttonArch = tk.Button(root, text="Arch Tome Calculator", width=18, command=lambda: open(1))
+    buttonArch.place(x=10, y=60)
 
     root.mainloop()
 
