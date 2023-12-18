3fee4d210f905ae988f44ec9701711d01cb3ef25
ew0345 <ew0345@users.noreply.github.com>
2023-12-17 23:27:30 -0500

    inital

---
+++ learn.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def init():
+    root = tk.Tk()
+    # Set Window Size
+    root.geometry("800x600")
+    # Set Window Title
+    root.title("Window")
+
+    # 
+    
+
+    root.mainloop()
+
+init()
